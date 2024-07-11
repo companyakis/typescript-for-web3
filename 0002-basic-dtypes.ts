9d7e21640cbfeafd708f3a193031ef675dee57b2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-11 22:02:36 +0300

    Create 0002-basic-dtypes.ts

---
+++ 0002-basic-dtypes.ts
@@ -0,0 +1,13 @@
+let myName: string
+
+myName = "Mustafa"
+
+let country: string = 'Turkiye'
+
+let expectedSalary: number 
+
+expectedSalary = 10000
+
+let isReady: boolean = true
+
+let isCompleted: boolean = false
