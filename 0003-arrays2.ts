1e5f09aca019881fa5ae70fa8d4882c9ca4eab66
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-11 22:18:11 +0300

    Create 0003-arrays2.ts

---
+++ 0003-arrays2.ts
@@ -0,0 +1,18 @@
+let employeeInfo = ["Aygün", "Finance", 35, 6500]
+
+let [firstName, department, age, salary] = employeeInfo
+
+console.log("First Name: " + firstName)
+
+console.log("Department: " + department)
+
+console.log("Age: " + age)
+
+console.log("Salary: " + salary)
+
+/*
+First Name: Aygün
+Department: Finance
+Age: 35
+Salary: 6500
+*/
