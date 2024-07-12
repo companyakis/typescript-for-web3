2c31d061e9863c005f2dbd2530d56b885d254a14
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-12 12:37:12 +0300

    Create 0007-enum2.ts

---
+++ 0007-enum2.ts
@@ -0,0 +1,10 @@
+enum CounterStrike {
+
+    Help = "Taking fire... Need assistance!",
+    Warning = "Enemy spotted",
+    Request = "Follow me!" 
+}
+
+console.log(CounterStrike.Help) // Taking fire... Need assistance!
+
+console.log(CounterStrike.Request) // Follow me!
