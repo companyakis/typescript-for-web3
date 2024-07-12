fd5ec8fd833f0803ae6de10c956526250d7b3965
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-12 12:27:08 +0300

    Create 0006-object2.ts

---
+++ 0006-object2.ts
@@ -0,0 +1,15 @@
+let employee : {
+    id: number,
+    name: string,
+    department: string,
+    salary: number
+}
+
+employee = {
+    id: 121,
+    name: "Bengü Atakan",
+    department: "Marketing",
+    salary: 6800
+}
+
+console.log(employee)
