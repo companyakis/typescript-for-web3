aeec7b35a7e8d3a6385b0963111a4ee9d45d61e5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-12 12:13:50 +0300

    Create 0004-tuple1.ts

---
+++ 0004-tuple1.ts
@@ -0,0 +1,11 @@
+let employee: [string, string, string, number, boolean]
+
+employee = ["Mustafa", "Büyükdereli", "Founder", 2024, true]
+
+console.log(employee) // [ 'Mustafa', 'Büyükdereli', 'Founder', 2024, true ]
+
+console.log(employee[2]) // Founder
+
+let student: [string, number] = ["Aygül Biricik", 2011]
+
+console.log(`Student age: ${2024 - student[1]}`) // Student age: 13
