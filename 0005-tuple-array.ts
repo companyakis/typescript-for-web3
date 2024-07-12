fc8005aa288c5999565ad9e584943df47e888570
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-07-12 12:17:11 +0300

    Create 0005-tuple-array.ts

---
+++ 0005-tuple-array.ts
@@ -0,0 +1,10 @@
+let employees: [number, string, string][] 
+
+employees = [
+    [1, "Aysel Gök", "HR"],
+    [2, "Kağan Bilir", "Sales"]
+]
+
+console.log(employees) // [ [ 1, 'Aysel Gök', 'HR' ], [ 2, 'Kağan Bilir', 'Sales' ] ]
+
+console.log(employees[1][1]) // Kağan Bilir
